9d032fb6f6a0f929c64b7262acdfc11e928aea6a
Benjamin Gramlich <benjamin@redwoolf.net>
2011-05-17 22:03:18 -0500

    fix Stanza parameters

--- src/main/scala/org/cephalosoft/scarlet/stanza/Stanza.scala
+++ src/main/scala/org/cephalosoft/scarlet/stanza/Stanza.scala
@@ -3,8 +3,8 @@ package org.cephalosoft.scarlet.stanza
 /**
  * Common stanza elements
  */
-abstract class Stanza(from: String, to: String, i: String) {
-  val mFrom = f
-  val mTo = t
-  val mId = i
+abstract class Stanza(from: String, to: String, id: String) {
+  val mFrom = from
+  val mTo = to
+  val mId = id
 }
