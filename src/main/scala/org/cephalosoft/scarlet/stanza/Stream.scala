78cb7eca2274274881a7b1bd725a81919b30cad1
Benjamin Gramlich <benjamin@redwoolf.net>
2011-05-17 21:39:41 -0500

    create stream object

---
+++ src/main/scala/org/cephalosoft/scarlet/stanza/Stream.scala
@@ -0,0 +1,11 @@
+package org.cephalosoft.scarlet.stanza
+
+import scala.xml._
+
+class Stream(version: String, from: String, to: String, id: String) extends Stanza(from, to, id) {
+
+}
+
+object Stream {
+
+}
